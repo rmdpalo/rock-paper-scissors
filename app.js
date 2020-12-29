8a495460a432016d697007888770ed67db0499ce
rmdpalo <rmd.palo@gmail.com>
2020-12-28 16:46:49 -0800

    create basic UI elements for player to use

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-const options = ["Rock", "Paper", "Scissors"]
+const options = ["Rock", "Paper", "Scissors"];
+let computerChoice = options[randomizedChoice()];
+
+//declare variables needed
+const buttons = document.querySelectorAll('button');
+const playerScoreboard = document.getElementById('#player-score');
+const cpuScoreboard = document.getElementById('#cpu-score');
+const results = document.getElementById('#results');
+
+let playerScore = 0;
+let computerScore = 0;
+//for each button we added an event listener, listening for a click.
+buttons.forEach((button) => {
+    //when clicked do this.
+    button.addEventListener('click', (e) => {
+        let playerChoice = button.id;
+        let computerChoice = options[randomizedChoice()];
+        console.log(playRound(playerChoice, computerChoice));
+    });
+    
+})
+
 
 let playerChoice;
-let computerChoice = options[randomizedChoice()];
 
 function randomizedChoice(){
     return Math.floor(Math.random() * options.length)
 }
 
 function playRound(playerChoice, computerChoice){
-    while(true) {
-        playerChoice = prompt("Rock, paper, or scissors?").toLowerCase();
-        if ((playerChoice === "rock") || (playerChoice === "paper") || (playerChoice === "scissors")) {
-            break;
-        }
-        alert("Please enter a valid choice.")
-    }
     computerChoice = computerChoice.toLowerCase();
 
     if(playerChoice === computerChoice){ //draw
@@ -28,37 +41,37 @@ function playRound(playerChoice, computerChoice){
     }
 }
 
+playerScoreboard.textContent = playerScore;
+cpuScoreboard.textContent = computerScore;
 let gameEnded = false;
-let playerScore = 0;
-let computerScore = 0;
 
-function resetGame (winner){
-    let reset = prompt("Play again? 'y' to restart.").toLowerCase();
-    if (reset === "y"){
-        playerScore = 0
-        computerScore = 0
-        gameEnded = false
-        game();
-    }
-    console.log(`Your score: ${playerScore}. Computer's Score: ${computerScore}.`)
-    console.log(`${winner} won the game!`)
-}
+// function resetGame (winner){
+//     let reset = prompt("Play again? 'y' to restart.").toLowerCase();
+//     if (reset === "y"){
+//         playerScore = 0
+//         computerScore = 0
+//         gameEnded = false
+//         game();
+//     }
+//     console.log(`Your score: ${playerScore}. Computer's Score: ${computerScore}.`)
+//     console.log(`${winner} won the game!`)
+// }
 
-function game(){
-    while(gameEnded === false){
-        console.log(`Your score: ${playerScore}. Computer's Score: ${computerScore}.`)
-        computerChoice = options[randomizedChoice()];
-        console.log(playRound(playerChoice, computerChoice));
-        if (playerScore === 5){
-            gameEnded = true;
-            resetGame("You");
-            return 
-        } else if (computerScore === 5) {
-            gameEnded = true;
-            resetGame("The computer");
-            return 
-        }
-    }
-}
+// function game(){
+//     while(gameEnded === false){
+//         console.log(`Your score: ${playerScore}. Computer's Score: ${computerScore}.`)
+//         computerChoice = options[randomizedChoice()];
+//         console.log(playRound(playerChoice, computerChoice));
+//         if (playerScore === 5){
+//             // gameEnded = true;
+//             resetGame("You");
+//             return 
+//         } else if (computerScore === 5) {
+//             // gameEnded = true;
+//             resetGame("The computer");
+//             return 
+//         }
+//     }
+// }
 
-console.log(game());
+// console.log(game());
